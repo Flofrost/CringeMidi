c3a2c29eb0c635c6db857387a78c9e722892cf03
Flofrost <ballagerflorian@gmail.com>
2023-01-29 17:14:34 +0100

    made buttons, yaya, need to fix fispay bug

--- src/CringeWidgets.py
+++ src/CringeWidgets.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 import curses as nc
 from abc import ABCMeta, abstractmethod
+from turtle import st
 
 from CringeMidi import *
 from CringeMisc import *
@@ -9,68 +10,116 @@ class Widget(metaclass=ABCMeta):
 
     def __init__(self,
                  screen:nc._CursesWindow,
-                 name:str=generateUID(),
-                 position: list[int, int] = [0,0]) -> None:
+                 name:str,
+                 position: list[int, int] = [0,0],
+                 color: int = 0) -> None:
         
         self.screen = screen
         self.name = name
         self.position = position
+        self.color = color
         
     @abstractmethod
     def draw(self):
         pass
+    
+class InteractibleWidget(Widget, metaclass=ABCMeta):
+    
+    def __init__(self,
+                 screen: nc._CursesWindow,
+                 name: str,
+                 position: list[int, int] = [0, 0],
+                 respondsTo: int = nc.BUTTON1_CLICKED,
+                 color: int = 0) -> None:
 
-class Button(Widget):
+        super().__init__(screen, name, position, color)
+
+        self.respondsTo = respondsTo
+        
+    @abstractmethod
+    def clicked(self, clickType:int, clickPosition:list[int, int]) -> bool:
+        pass
+
+class ToggleButton(InteractibleWidget):
 
     def __init__(self,
-                 screen: nc._CursesWindow, 
-                 name: str = generateUID(),
-                 text: str = "Button",
+                 screen: nc._CursesWindow,
+                 name: str,
                  position: list[int, int] = [0, 0],
-                 respondsTo: tuple = (nc.BUTTON1_CLICKED,None),
-                 style: str = "text") -> None:
+                 respondsTo: int = nc.BUTTON1_CLICKED,
+                 text: str = "Button",
+                 style: str = "text",
+                 color: int = 0) -> None:
 
-        super().__init__(screen, name, position)
+        super().__init__(screen, name, position, respondsTo, color)
 
         if style == "text":
             size = [len(text), 1]
+        elif style == "bordered":
+            size = [len(text) + 2, 3]
         else:
             raise Exception(f"'{style}' is not a recognized style")
 
         self.size = size 
         self.text = text
         self.style = style
+        self.color = color
         self.respondsTo = respondsTo
+        self.state = False
+        
+        self.draw()
         
     def draw(self):
+        color = (self.color | nc.A_REVERSE) if self.state else self.color
         if self.style == "text":
-            self.screen.addstr(self.position[1], self.position[0], self.text)
+            self.screen.addstr(self.position[1], self.position[0], self.text, color)
+        elif self.style == "bordered":
+            topbot = "─" * len(self.text)
+            self.screen.addstr(self.position[1], self.position[0], "┌" + topbot + "┐", color)
+            self.screen.addstr(self.position[1] + 1, self.position[0], "│" + self.text + "│", color)
+            self.screen.addstr(self.position[1] + 2, self.position[0], "└" + topbot + "┘", color)
 
-    def cliked(self, clickPosition:list[int, int], clickType:int) -> bool:
-        for response in self.respondsTo:
-            if clickType == response:
-                relPos = subPos(clickPosition, self.position)
-                return (relPos[0] >= 0) and (relPos[1] >= 0) and (relPos[0] < self.size[0]) and (relPos[1] < self.size[1])
+    def clicked(self, clickType: int, clickPosition: list[int, int]) -> bool:
+        if clickType == self.respondsTo:
+            relPos = subPos(clickPosition, self.position)
+            if (relPos[0] >= 0) and (relPos[1] >= 0) and (relPos[0] < self.size[0]) and (relPos[1] < self.size[1]):
+                self.state = not self.state
+                self.draw()
+                return True
         return False
     
 class StatusBar(Widget):
 
-    def __init__(self,
+    def __init__(self, 
                  screen: nc._CursesWindow,
-                 name: str = generateUID(),
+                 name: str,
+                 position: list[int, int] = [0, 0],
                  text: str = "",
-                 color = 0,
-                 justification: str = "left") -> None:
-
-        super().__init__(screen, name, [0, screen.getmaxyx()[0]-1])
+                 justification: str = "left",
+                 color: int = 0) -> None:
         
-        self.size = [screen.getmaxyx()[1], 1]
+        super().__init__(screen, name, position, color)
+
         self.text = text
         self.color = color
         self.justification = justification
+        
+        self.draw()
         
     def draw(self):
         self.position = [0, self.screen.getmaxyx()[0]-1]
         self.screen.move(self.position[1], self.position[0])
         self.screen.clrtoeol()
-        self.screen.addstr(self.position[1], self.position[0], self.text, nc.color_pair(self.color))
+
+        if self.justification == "left":
+            text = self.text + " " * (self.screen.getmaxyx()[1] - len(self.text) - 1)
+            self.screen.addstr(self.position[1], self.position[0], text, nc.color_pair(self.color) | nc.A_REVERSE)
+        elif self.justification == "center":
+            textMid = len(self.text) // 2
+            screenMid = self.screen.getmaxyx()[1] // 2
+            text = " " * (screenMid - textMid - 1) + self.text + " " * (screenMid - textMid - 1)
+            self.screen.addstr(self.position[1], 0, text, nc.color_pair(self.color) | nc.A_REVERSE)
+        
+    def updateText(self,text: str):
+        self.text = text
+        self.draw()
--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 #!/bin/python3
 from __future__ import annotations
 import curses as nc
-from tkinter import Button
 
 import CringeMidi
 from CringeMisc import *
@@ -11,18 +10,49 @@ def main(screen:nc._CursesWindow):
 
     if nc.has_colors():
         nc.use_default_colors()
+        nc.init_pair(1, 135, -1)
+
     nc.mousemask(-1)
 
     screen.nodelay(1)
-
-    delay = 1000
-    mode = "normal"
+    screen.timeout(50)
+    
+    modes = ["Normal", "Insert", "Visual", "Play"]
+    mode = modes[0]
+    i = 0
     screenSize = screen.getmaxyx()
     
+    listOfAllInteractibleWidgets: CringeWidgets.InteractibleWidget = [
+        CringeWidgets.ToggleButton(screen=screen,
+                                   name=modes[0],
+                                   position=[0,0],
+                                   text=" Normal",
+                                   style="bordered"),
+        CringeWidgets.ToggleButton(screen=screen,
+                                   name=modes[1],
+                                   position=[10,0],
+                                   text=" Insert",
+                                   style="bordered"),
+        CringeWidgets.ToggleButton(screen=screen,
+                                   name=modes[2],
+                                   position=[20,0],
+                                   text="麗 Visual",
+                                   style="bordered"),
+        CringeWidgets.ToggleButton(screen=screen,
+                                   name=modes[3],
+                                   position=[30,0],
+                                   text="金 Play",
+                                   style="bordered"),
+    ]
+
     statusBar = CringeWidgets.StatusBar(screen=screen,
-                                        text="Testing...")
-    
-    statusBar.draw()
+                                        name="statusBar",
+                                        justification="left",
+                                        color=1)
+
+    listOfAllWidgetsInDaProgram: CringeWidgets.Widget = [
+        statusBar
+    ] + listOfAllInteractibleWidgets
 
     while True:
         
@@ -33,14 +63,19 @@ def main(screen:nc._CursesWindow):
             eventPosition = event[1:3]
             event = event[4]
             
-            if event == nc.BUTTON1_TRIPLE_CLICKED:
-                return
+            for widget in listOfAllInteractibleWidgets:
+                if widget.clicked(event, eventPosition):
+                    pass
 
         if nc.is_term_resized(screenSize[0], screenSize[1]):
+            screen.clear()
             screenSize = screen.getmaxyx()
-            statusBar.draw()
-
-        nc.napms(delay)
+            
+            for widget in listOfAllWidgetsInDaProgram:
+                widget.draw()
+            
+        i += 1
+        statusBar.updateText(f"Counter = {i}")
 
 if __name__ == "__main__":
     nc.wrapper(main)
